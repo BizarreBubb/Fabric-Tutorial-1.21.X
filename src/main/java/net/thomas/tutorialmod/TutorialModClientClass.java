4c0e161d477207b105b4b81e1545cd07bc57db23
BizarreBubb <thomasnguyen69420@gmail.com>
2024-10-19 22:06:02 -0700

    added commits

---
+++ src/main/java/net/thomas/tutorialmod/TutorialModClientClass.java
@@ -0,0 +1,10 @@
+package net.thomas.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClientClass implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
